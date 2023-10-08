950fc239b1f0548d04e57c48c0a71f33371a5171
VALENTIN YE LI <yeliv9527@gmail.com>
2023-10-08 00:14:34 -0300

    plot desicion boundary for the perceptron

--- TP3/Exercise1.py
+++ TP3/Exercise1.py
@@ -1,19 +1,71 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from sklearn import datasets
 
-# Generates linearly separable data
+import os
+
+from SimplePerceptron import SimplePerceptron
+
+# Create the output folder if it doesn't exist
+if not os.path.exists("Output/ex1"):
+    os.makedirs("Output/ex1/")
+
+# Generates linearly separable data 
 def generate_linearly_separable_data(n_samples):
-    np.random.seed(0)
-    X = np.random.uniform(0, 5, (n_samples, 2))
-    y = np.where(X[:, 0] > X[:, 1], 1, -1)
+    X, y = datasets.make_blobs(n_samples=n_samples, n_features=2, centers=2, cluster_std=1.05, random_state=42)
     return X, y
 
-X, y = generate_linearly_separable_data(100)
+def generate_not_so_linearly_separable_data(n_samples):
+    X, y = datasets.make_blobs(n_samples=n_samples, n_features=2, centers=2, cluster_std=3, random_state=42)
+    return X, y
+
+X, y = generate_linearly_separable_data(50)
+y = np.where(y == 0, -1, 1)
 
 # Plot the data
 plt.scatter(X[:, 0], X[:, 1], c=y)
 plt.title("Linearly separable data")
-plt.xlabel("X1")
-plt.ylabel("X2")
+plt.xlabel("X")
+plt.ylabel("Y")
+plt.savefig("Output/ex1/linearly_separable_data.png")
 plt.show()
 
+
+
+# Train the perceptron
+perceptron = SimplePerceptron(learning_rate=0.01, epochs=1000, accepctable_error=0.01)
+epochs, error, errors = perceptron.fit(X, y)
+
+print("Epochs: ", epochs)
+print("Errors", errors)
+
+
+# Plot the error over the epochs
+plt.plot(range(epochs), errors)
+plt.title("Error over the epochs")
+plt.xlabel("Epoch")
+plt.ylabel("Error")
+plt.savefig("Output/ex1/error_over_the_epochs.png")
+plt.show()
+
+
+# Plot decision boundary
+# w1x1 + w2x2 + b = 0 -> x2 = (-w1x1 - b) / w2
+
+ax = plt.subplot(1, 1, 1)
+plt.scatter(X[:, 0], X[:, 1], c=y)
+
+x0_1 = np.amin(X[:, 0])
+x0_2 = np.amax(X[:, 0])
+
+x1_1 = (-perceptron.weights[0] * x0_1 - perceptron.bias) / perceptron.weights[1]
+x1_2 = (-perceptron.weights[0] * x0_2 - perceptron.bias) / perceptron.weights[1]
+
+ymin = np.amin(X[:, 1])
+ymax = np.amax(X[:, 1])
+ax.set_ylim([ymin-3, ymax+3])
+
+ax.plot([x0_1, x0_2], [x1_1, x1_2], 'k')
+
+plt.savefig("Output/ex1/decision_boundary.png")
+plt.show()
--- TP3/SimplePerceptron.py
+++ TP3/SimplePerceptron.py
@@ -1,18 +1,19 @@
 import numpy as np
 
-def _unit_step_func(self, x):
+def _unit_step_func(x):
     return np.where(x>=0, 1, 0)
 
 class SimplePerceptron:
-
-    def __init__(self, learning_rate = 0.01, n_iters=1000):
+    def __init__(self, learning_rate = 0.01, epochs=1000, accepctable_error=0.001):
         self.lr = learning_rate
-        self.n_iters = n_iters
+        self.epochs = epochs
         self.activation_func = _unit_step_func
+        self.accepctable_error = accepctable_error
         self.weights = None
         self.bias = None
 
     def fit(self, X, y):
+        errors = []
         n_samples, n_features = X.shape
 
         # init weights
@@ -22,7 +23,7 @@ class SimplePerceptron:
         y_ = np.where(y > 0, 1, 0)
 
         # learning
-        for _ in range(self.n_iters):
+        for _ in range(self.epochs):
             for idx, x_i in enumerate(X):
                 linear_output = np.dot(x_i, self.weights) + self.bias
                 y_predicted = self.activation_func(linear_output)
@@ -30,10 +31,24 @@ class SimplePerceptron:
                 update = self.lr * (y_[idx] - y_predicted)
                 self.weights += update * x_i
                 self.bias += update
+            
+            actual_error = self.error(X, y)
+            errors.append(actual_error)
+            if actual_error <= self.accepctable_error:
+                print("Converged at epoch: ", _)
+                return _ + 1, actual_error, errors
+        print("Did not converge")
+        return self.epochs, actual_error, errors     
     
     def predict(self, X):
+        # hyperplane equation
         linear_output = np.dot(X, self.weights) + self.bias
         y_predicted = self.activation_func(linear_output)
         return y_predicted
+    
+    def error(self, X, y):
+        y_ = np.where(y > 0, 1, 0)
+        y_predicted = self.predict(X)
+        return np.mean(np.abs(y_ - y_predicted))
 
         
